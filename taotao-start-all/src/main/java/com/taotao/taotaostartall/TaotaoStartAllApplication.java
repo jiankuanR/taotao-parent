662900817dfb72dfc62582021e9d77a4389bcd17
luowei <luowei@cloudwalk.cn>
2019-01-11 23:09:56 +0800

    Initial commit

---
+++ taotao-start-all/src/main/java/com/taotao/taotaostartall/TaotaoStartAllApplication.java
@@ -0,0 +1,14 @@
+package com.taotao.taotaostartall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaotaoStartAllApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaotaoStartAllApplication.class, args);
+    }
+
+}
+
